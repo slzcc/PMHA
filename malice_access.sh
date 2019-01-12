7707aea4950f0f198d21b98d620c73ee0584ac2b
root <root@hotstone.com.cn>
2019-01-12 13:55:10 +0800

    fix bug

--- malice_access.sh
+++ malice_access.sh
@@ -2,18 +2,20 @@
 
 workDir="/root"
 project="PMHA"
+sqe_number=8
 
-HostIP="101.200.83.130"
+HostIP="47.95.219.151"
 
 CapFile="/tmp/`date +%F:%H:%M.cap`"
 ReIPFile="/tmp/`date +%F:%H:%M`_ReIPList.txt"
 
-tcpdump -i eth0 -nn host ${HostIP} and ! port 22 and ! icmp -c 2000  >>  ${CapFile}
+tcpdump -nn -i eth0 ! port 22 -c 7000 >>  ${CapFile}
+#tcpdump -i eth0 -nn host ${HostIP} and ! port 22 and ! icmp -c 2000  >>  ${CapFile}
 
 cat ${CapFile} | grep '\[S\]'  | awk '{print $3}'| awk -F. 'OFS="."{$NF="";print}'|sed 's/\.$//' | egrep "^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$" | sort|uniq -d -c > ${ReIPFile}
 
 cat ${ReIPFile} | while read line  ; do
-  if [[ `echo $line | awk '{print $1}'` -ge 20 ]]; then
+  if [[ `echo $line | awk '{print $1}'` -ge ${sqe_number} ]]; then
     echo $line | awk '{print $2}' >> ${workDir}/${project}/.new_ip_list.txt;
   fi;
 done
